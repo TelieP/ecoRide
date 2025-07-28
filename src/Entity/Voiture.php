c98767855416dcda882cbff758dfb6087367e389
Parfait Fongang <parfaittelie@outlook.fr>
2025-07-28 11:41:46 +0200

    création du crud pour les entités Marque et Voiture

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modele = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $immatriculation = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $energie = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $couleur = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $datePremiereImmatriculation = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Marque $marque = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): static
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getEnergie(): ?string
+    {
+        return $this->energie;
+    }
+
+    public function setEnergie(string $energie): static
+    {
+        $this->energie = $energie;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getDatePremiereImmatriculation(): ?\DateTime
+    {
+        return $this->datePremiereImmatriculation;
+    }
+
+    public function setDatePremiereImmatriculation(\DateTime $datePremiereImmatriculation): static
+    {
+        $this->datePremiereImmatriculation = $datePremiereImmatriculation;
+
+        return $this;
+    }
+
+    public function getMarque(): ?Marque
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(Marque $marque): static
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+}
