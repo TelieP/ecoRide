fd02cc91a2958282d2f2fb69ec3e4e89a6ee65e7
Parfait Fongang <parfaittelie@outlook.fr>
2025-07-31 13:50:38 +0200

    modification du HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +12,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(VoitureRepository $voitureRepository): Response
     {
+        $voitures = $voitureRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'voitures' => $voitures,
