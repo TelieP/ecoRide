8b1083262ee2598e70dcb2fa899e6112dad86030
Parfait Fongang <parfaittelie@outlook.fr>
2025-07-31 12:59:39 +0200

    ajout boucle for pour recupérer les voitures en BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(VoitureRepository $voitureRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'voitures' => $voitures,
         ]);
     }
 }
