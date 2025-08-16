cf51ee2b7e118f6973cf558b8449413b14b58488
Parfait Fongang <parfaittelie@outlook.fr>
2025-08-16 22:24:48 +0200

    création de la relation entre Voiture et Marque

--- src/Entity/Marque.php
+++ src/Entity/Marque.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MarqueRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MarqueRepository::class)]
@@ -13,8 +15,43 @@ class Marque
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var Collection<int, Voiture>
+     */
+    #[ORM\ManyToMany(targetEntity: Voiture::class)]
+    private Collection $voiture;
+
+    public function __construct()
+    {
+        $this->voiture = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Voiture>
+     */
+    public function getVoiture(): Collection
+    {
+        return $this->voiture;
+    }
+
+    public function addVoiture(Voiture $voiture): static
+    {
+        if (!$this->voiture->contains($voiture)) {
+            $this->voiture->add($voiture);
+        }
+
+        return $this;
+    }
+
+    public function removeVoiture(Voiture $voiture): static
+    {
+        $this->voiture->removeElement($voiture);
+
+        return $this;
     }
 }
