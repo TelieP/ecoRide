91bbab192ff63b12a648dd346aac7367bff386a7
Parfait Fongang <parfaittelie@outlook.fr>
2025-10-09 07:07:26 +0200

    mise à jour des formulaires de connexion et de la page d'accueil

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -11,26 +11,25 @@ use Symfony\Component\Security\Core\Security;
 
 final class UtilisateurController extends AbstractController
 {
-    #[Route('/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
-    {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
-    }
-     #[Route('/profil', name: 'app_user_profile')]
-    #[IsGranted('ROLE_USER')]
-    public function profile(CovoiturageRepository $covoiturageRepository): Response
-    {
-        $user = $this->getUser();
+  #[Route('/utilisateur', name: 'app_utilisateur')]
+  public function index(): Response
+  {
+    return $this->render('utilisateur/index.html.twig', [
+      'controller_name' => 'UtilisateurController',
+    ]);
+  }
+  #[Route('/profil', name: 'app_user_profile')]
+  #[IsGranted('ROLE_USER')]
+  public function profile(
+    CovoiturageRepository $covoiturageRepository,
+  ): Response {
+    $user = $this->getUser();
 
-        // Récupérer les trajets proposés par l'utilisateur
-        $trajetsProposes = $covoiturageRepository->findBy(['user' => $user]);
+    $trajetsProposes = $covoiturageRepository->findBy(['user' => $user]);
 
-        return $this->render('user/profile.html.twig', [
-            'user' => $user,
-            'trajets_proposes' => $trajetsProposes,
-            // 'trajets_reserves' => $trajetsReserves, // Décommenter si vous avez cette relation
-        ]);
-    }
+    return $this->render('user/profile.html.twig', [
+      'user' => $user,
+      'trajets_proposes' => $trajetsProposes,
+    ]);
+  }
 }
